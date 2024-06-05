b46730976cea5baacdf7a0f10419117fd2454781
prestonm44 <preston.mitchell4444@gmail.com>
2024-06-04 18:55:12 -0600

    API working

--- public/nfl-players.js
+++ public/nfl-players.js
@@ -1,7 +1,6 @@
-import { response } from 'express';
-import axios from 'axios';
+
 
 axios.get('https://api.sportsdata.io/v3/nfl/scores/json/PlayersBasic/SF?key=53ac94d7422e4a65b18e03b16aeb7418')
     .then( (response) => {
-        document.querySelector('#apiText').innerText = response.data;
+        document.querySelector('#apiText').innerText = JSON.stringify(response.data);
     })
